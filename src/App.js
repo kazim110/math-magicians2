c3d832382a2d0bc8e6229a0b0017d13f50b08aff
kazim <kazim.mohammadi110@gmail.com>
2023-08-12 12:59:43 +0430

    add quote api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Calculator from './components/Calculator';
 import './components/css/Calculator.css';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <section className="container">
+        <Quote />
         <Calculator />
       </section>
     </div>
